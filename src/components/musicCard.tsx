5712c6d8d95aa86dd6227f9bdc67c9c8c9078c18
Izadora Braga <izadora_abraga@outlook.com>
2023-11-09 23:54:48 -0300

    requisito 5 finalizado

--- src/components/musicCard.tsx
+++ src/components/musicCard.tsx
@@ -1,19 +1,46 @@
-import { SongType } from '../types';
+import React, { useState } from 'react';
+import emptyHeart from '../images/empty_heart.png';
+import checkedHeart from '../images/checked_heart.png';
 
 interface MusicCardProps {
-  music: SongType;
+  music: {
+    trackId: number;
+    trackName: string;
+    previewUrl: string;
+  };
 }
 
 function MusicCard({ music }: MusicCardProps) {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const { trackId, trackName, previewUrl } = music;
+
+  const handleCheckboxChange = () => {
+    setIsFavorited(!isFavorited);
+  };
+
   return (
     <div>
-      <p>{music.trackName}</p>
-      <audio data-testid="audio-component" src={ music.previewUrl } controls>
+      <p>{trackName}</p>
+      <audio data-testid="audio-component" src={ previewUrl } controls>
         <track kind="captions" />
         O seu navegador não suporta o elemento
         <code>audio</code>
         .
       </audio>
+      <label data-testid={ `checkbox-music-${trackId}` }>
+        <input
+          type="checkbox"
+          checked={ isFavorited }
+          onChange={ handleCheckboxChange }
+          style={ { display: 'none' } }
+        />
+        <img
+          src={ isFavorited ? checkedHeart : emptyHeart }
+          alt="favorite"
+          style={ { width: '20px', height: '20px' } }
+        />
+      </label>
     </div>
   );
 }
